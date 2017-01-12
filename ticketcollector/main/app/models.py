df59df6289752a8ed0b97261f70e6d024a1883da
Jijoy <jijoy21@gmail.com>
2017-01-12 10:40:32 +0530

    Bug fixes , #5 and duplicate filter of #4

--- ticketcollector/main/app/models.py
+++ ticketcollector/main/app/models.py
@@ -9,3 +9,26 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Collection(models.Model):
+    collection_id = models.AutoField(primary_key=True)
+    search_criteria = models.CharField(max_length=2000,null=True)
+    name = models.CharField(max_length=1000,unique=True)
+
+
+class Ticket(models.Model):
+    ticket_id = models.AutoField(primary_key=True)
+    collection = models.ForeignKey(Collection,related_name="collection")
+    zd_ticket_id = models.CharField(max_length=1000)
+    subject = models.CharField(max_length=1000)
+    requester = models.CharField(max_length=1000)
+    description = models.TextField()
+    created_at  = models.DateTimeField()
+
+class Comments(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    ticket = models.ForeignKey(Ticket,related_name="ticket")
+    zd_comment_id = models.CharField(max_length=1000)
+    created_at = models.DateTimeField()
+    plain_body = models.TextField()
+    is_public = models.BooleanField()
--- ticketcollector/main/app/views.py
+++ ticketcollector/main/app/views.py
@@ -1,6 +1,5 @@
 import json
 
-import toolz
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
