d9e8af21885b200e6dec831e58a00ea361cc1729
Jijoy <jijoy21@gmail.com>
2017-01-12 10:27:46 +0530

    Bug fixes , #5 and duplicate filter of #4

--- ticketcollector/main/app/views.py
+++ ticketcollector/main/app/views.py
@@ -6,7 +6,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-import toolz
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
