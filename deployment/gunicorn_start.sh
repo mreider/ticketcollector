2ca744c919210d6fd97ec1ed2b633e06853568e1
Jijoy <jijoy21@gmail.com>
2017-01-19 17:09:09 +0530

    Deployment config added

---
+++ deployment/gunicorn_start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NAME="ticketcollector"                                  # Name of the application
+DJANGODIR=/webapp/ticketcollector/ticketcollector/main             # Django project directory
+SOCKFILE=/webapp/run/gunicorn.sock  # we will communicte using this unix socket
+NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=main.settings             # which settings file should Django use
+DJANGO_WSGI_MODULE=main.wsgi                     # WSGI module name
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source ../../env/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec ../../env/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --bind=unix:$SOCKFILE \
+  --log-level=debug \
+  --log-file=-
